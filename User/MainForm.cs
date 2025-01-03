0d734006359300212c624a4d46e5c5fdb8dd8d5c
Nikola Markovic <nikola.markovicmc@gmail.com>
2025-01-03 20:07:49 +0100

    6: prijava na sistem s podacima iz UserAccounts.txt; dodatno ciscenje backenda

--- User/MainForm.cs
+++ User/MainForm.cs
@@ -1,23 +1,30 @@
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
+using System.Windows.Forms.Design;
 using System.Windows.Forms.VisualStyles;
 using User;
 
+#nullable disable
 namespace User {
     public sealed partial class MainForm : Form, Interfaces.IInitializable {
-        public static bool consentGiven = false;
+        // debug parametri za preskakanje prva dva prozora;
+        // ocekuje se da ce finalna verzija zahtijevati (barem) prijavu pri svakom pokretanju
+        private static bool consentGiven = true;
+        private static bool isLoggedIn = false;
+
+        // da li postoji aktivni tiket; potencijalno kreirati posebnu strukturu za dati tiket
         public static bool activeTicketExists = false;
 
-        private readonly ConsentForm cf = new();
         private readonly Constants constants = new();
         private readonly FileManager fm = new();
 
+        // IInitializable
         public void SetDefaultWindowSettings() {
             FormBorderStyle = FormBorderStyle.FixedSingle;
         }
 
         public void FillStaticConstants() {
-            Text = constants.GetMainFormTitle("FIRMNAME");
+            Text = constants.GetFormWindowTitle("FIRMNAME");
         }
 
         public void SetDefaultStates() {
@@ -25,30 +32,58 @@ namespace User {
             respondButton.Enabled = false;
             archivedTicketsButton.Enabled = true;
             refreshButton.Enabled = true;
+            accountNameLabel.Text = "Nalog: " + LoginForm.loggedInAccountUsername;
         }
+        // #IInitializable
+
+        private void FailedLoginCloseOnStart(object sender, EventArgs e) => Close();
 
         public MainForm() {
+            // ako korisnik nije vec prijavljen, prikazati formu za prijavu
+            if (!isLoggedIn)
+                new LoginForm().ShowDialog();
+            else
+                LoginForm.loginSuccessful = true;
+            // ^ else je za debug, koristi placeholdere za ime naloga
+
+            // ako je korisnik neuspjesno prijavljen (najcesce forma za prijavu zatvorena na X),
+            // dodati funkciju za zatvaranje koja se poziva po zavrsetku konstruktora
+            if (!LoginForm.loginSuccessful)
+                Load += FailedLoginCloseOnStart;
+            else {
+                // inace prikazati formu za saglasnost; ovo funkcionise na ovaj nacin jer, kao sto
+                // sam naveo u komentaru iznad, konstrukcija se vrsi do kraja, a (ja mislim da)
+                // instanca ConsentForm-a komunicira sa MainForm nakon sto se pozove lista funkcija
+                // obuhvacena u Load, tako da ConsentForm pokusava da pristupi dealociranoj instanci
+                // MainForm-a
+                if (!consentGiven)
+                    new ConsentForm().ShowDialog();
+            }
+
             InitializeComponent();
             SetDefaultWindowSettings();
             FillStaticConstants();
             SetDefaultStates();
 
-            if (!consentGiven)
-                cf.ShowDialog();
+            accountNameLabel.Text = "Nalog: " + LoginForm.loggedInAccountUsername;
         }
 
+        // "Napravi tiket" dugme
         private void createTicket_click(object sender, EventArgs e) {
 
         }
 
+        // "Odgovori" dugme
         private void respondButton_Click(object sender, EventArgs e) {
 
         }
 
+        // "Arhivirani tiketi" dugme
         private void archivedTicketsButton_Click(object sender, EventArgs e) {
-
+            
         }
 
+        // "Refresh" dugme
         private void refreshButton_Click(object sender, EventArgs e) {
 
         }
--- User/Program.cs
+++ User/Program.cs
@@ -8,6 +8,10 @@ namespace User {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+
+            // program je prvobitno zavisio od LoginForm, medjutim nisam pronasao nacin da
+            // zatvorim glavni LoginForm kada zatvaram MainForm pa bi program ostao raditi
+            // u pozadini
             Application.Run(new MainForm());
         }
     }

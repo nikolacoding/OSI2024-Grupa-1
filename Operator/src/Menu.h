2f9cf645a7b1af88827af4727e787d5bde26e994
Nikola Markovic <nikola.markovicmc@gmail.com>
2025-01-10 14:30:10 +0100

    standardizacija podataka u operaterskom dijelu nalik na administratorski

--- Operator/src/Menu.h
+++ Operator/src/Menu.h
@@ -1,14 +1,15 @@
-
 #include <string>
 #include <vector>
+
+#include "Data.h"
 
 class Menu {
 private:
     std::string operatorName;
-    std::vector<std::string> tickets;
+    std::vector<TicketData> tickets;
 
 public:
-    Menu(const std::string& name, const std::vector<std::string>& ticketList);
+    Menu(const std::string& name, const std::vector<TicketData>& ticketList);
     void displayMenu() const;
     void runMenu();
 };
